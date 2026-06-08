824773ea307a068cb44fda348b18117299408fa0
Kauany157 <126580560+Kauany157@users.noreply.github.com>
2026-06-07 22:42:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,47 +2,48 @@
 #include "functions.h"
 
 int main(void) {
-    int opcao, saiu = 0;
+    int ler, saiu = 0;
 
     do {
         menu();
 
-        if (scanf("%d", &opcao) != 1) {
+        if (scanf("%d", &ler) != 1) {
             scanf("%*[^\n]");
             getchar();
-            printf("Comando invalido.\n");
+            printf("Comando invalido, por favor, tente novamente.\n");
             pausar();
             continue;
         }
+
         scanf("%*[^\n]");
         getchar();
 
-        if (opcao >= 1 && opcao <= 4) {
-            if (!confirmarOpcao1(opcao)) {
+        if (ler >= 1 && ler <= 4) {
+            if (!confirmarOpcao1(ler)) {
                 pausar();
                 continue;
             }
         }
 
-        switch (opcao) {
+        switch (ler) {
             case 1:
-               gerenciarAbelhas();
+                gerenciarAbelhas();
                 break;
             case 2:
                 gerenciarSensores();
                 break;
             case 3:
-               gerenciarRelatorios();
+                gerenciarRelatorios();
                 break;
             case 4:
                 printf("Voce saiu.\n");
                 saiu = 1;
                 break;
             default:
-                printf("Comando invalido.\n");
+                printf("Comando invalido, por favor, tente novamente.\n");
                 pausar();
         }
-    } while (!saiu);
+    } while (saiu == 0);
 
     return 0;
 }
